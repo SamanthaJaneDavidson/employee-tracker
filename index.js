7a341f6f99e726425fd493904a36d1a11c57ec3e
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-11-24 14:05:56 -0500

    Added inquirer prompts to index

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const inquirer = require("inquirer");
 const connection = require('./db/connection.js');
+const Department = require("./lib/department.js");
+const Employee = require("./lib/employee.js");
+const Role = require("./lib/role.js");
 
+let departmentList = [];
+let employeeList = [];
+let roleList = [];
 
 const addDepartment = () => {
     inquirer.prompt([
@@ -12,9 +18,66 @@ const addDepartment = () => {
     ])
 
         .then(answers => {
-            const newDepartment = new Department(answers.internName, answers.id, answers.email, answers.school)
-            employeeList.push(newIntern);
-            menu();
+            const newDepartment = new Department(answers.deptName)
+            departmentList.push(newDepartment);
+            mainMenu();
+        })
+};
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "What is the employee's first name?",
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the employee's last name?",
+        },
+        {
+            type: "input",
+            name: "roleId",
+            message: "What is the employee's role ID?",
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the employee manager's ID?",
+        },
+    ])
+
+        .then(answers => {
+            const newEmployee = new Employee(answers.firstName, answers.lastName, answers.roleId, answers.managerId)
+            employeeList.push(newEmployee);
+            mainMenu();
+        })
+};
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title?",
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the salary?",
+        },
+        {
+            type: "input",
+            name: "departmentId",
+            message: "What is the department ID?",
+        },
+    ])
+
+        .then(answers => {
+            const newRole = new Role(answers.title, answers.salary, answers.departmentId)
+            roleList.push(newRole);
+            mainMenu();
         })
 };
 
@@ -25,7 +88,7 @@ const program_exit = () => {
     connection.end();
 }
 
-const mainmenu = () => {
+const mainMenu = () => {
     return inquirer.prompt([
         {
             type: "list",
@@ -63,4 +126,4 @@ const mainmenu = () => {
         });
 };
 
-mainmenu();
+mainMenu();
