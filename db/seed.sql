862bf6c097c21f09201eab21b4244ce95a5a3f97
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-11-24 13:53:18 -0500

    Updated seed data

--- db/seed.sql
+++ db/seed.sql
@@ -1,49 +1,24 @@
 INSERT INTO department
 (department_name)
 VALUES 
-("Accounting", "Human Resources", "Project Management", "Operations")
+("Accounting"),
+("Human Resources"),
+("Project Management"), 
+("Operations");
 
 
 INSERT INTO role
-(title, salary, department)
+(title, salary, department_id)
 VALUES
-('Manager',
-'125000',
-'Accounting'
-),
-('Director',
-'Human Resources',
-'150000'
-),
-('Business Analyst',
-'Project Management',
-'125000'
-),
-('Operations',
-'Program Manager',
-'100000'
-)
+('Manager','125000', 1),
+('Director','150000', 4),
+('Business Analyst','125000', 2),
+('Program Manager','100000', 3);
 
 INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
 VALUES
-('Sam',
-'Davidson',
-'1' -- need to be relational to the other tables
-'2' -- need to be relational to the other tables
-),
-('Anthony',
-'Maddatu',
-'3'
-'4'
-),
-('Tom',
-'Woods',
-'5'
-'6'
-),
-('Bob',
-'Builder',
-'7'
-'8'
-)
+('Sam','Davidson','1', 2),
+('Anthony','Maddatu','3','4'),
+('Tom','Woods','5','6'),
+('Bob','Builder','7','8');
